bf71a9f87195d4896b925fd53c6a37bfe5f7f71d
Renat Babacaev <renat.babacaev@gmail.com>
2024-11-14 11:42:03 +0200

    Commit current changes, more work pending.

---
+++ src/About.jsx
@@ -0,0 +1,16 @@
+function About() {
+  return (
+    <>
+      <div class="flex flex-col justify-evenly mx-2 min-h-screen">
+        <div class="mx-auto">
+          <p class="text-xl text-justify">
+            Temnițe și Dragoni MD este o grupă colectivă de jucători 
+            formată din voluntari și jucători care au ca scop promovarea 
+            jocului de masă Dungeons and Dragons.</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default About;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,14 @@
 function Home() {
-
-return (
+  return (
     <>
-      <div class="m-8 flex flex-col flex-grow">
-        <img src="./src/assets/logo.png" alt="Temnițe și Dragoni" />
-        <p class="text-xl text-center font-light tracking-wide dark:text-white">Trăim mii de aventuri împreună!</p>
-      </div>
-      <div class="mb-8 mx-auto px-8 justify-center">
-        <img src="./src/assets/iframe_example.png" alt="ig" />
+      <div class="flex flex-col justify-evenly mx-2 min-h-screen">
+        <div class="flex flex-col flex-grow">
+          <img src="./src/assets/logo.png" alt="Temnițe și Dragoni" />
+          <p class="text-xl text-center font-light tracking-wide">Trăim mii de aventuri împreună!</p>
+        </div>
+        <div class="mb-8 mx-auto px-8 justify-center">
+          <img src="./src/assets/iframe_example.png" alt="ig" />
+        </div>
       </div>
     </>
   )

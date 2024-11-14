f93f93476c89c58a869e089801c109b048379dbf
Renat Babacaev <renat.babacaev@gmail.com>
2024-11-14 11:55:37 +0200

    Change logo to a more vivid svg one

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,7 @@ function Home() {
     <>
       <div class="flex flex-col justify-evenly mx-2 min-h-screen">
         <div class="flex flex-col flex-grow">
-          <img src="./src/assets/logo.png" alt="Temnițe și Dragoni" />
+          <img src="./src/assets/logo.svg" alt="Temnițe și Dragoni" />
           <p class="text-xl text-center font-light tracking-wide">Trăim mii de aventuri împreună!</p>
         </div>
         <div class="mb-8 mx-auto px-8 justify-center">

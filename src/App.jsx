1f70d42f4e9de1863f6edbf150596ac72a1152d5
Renat Babacaev <renat.babacaev@gmail.com>
2024-11-14 11:42:03 +0200

    Commit current changes, more work pending.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Home from './Home.jsx'
+import About from './About.jsx'
 
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Home />
+      <About />
     </>
   )
 }

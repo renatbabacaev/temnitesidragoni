86162c2a3c3414027884fe44e60bf7be48fcbb15
Renat Babacaev <renat.babacaev@gmail.com>
2024-12-13 12:05:25 +0200

    Remove unecessary classes

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,14 +1,14 @@
 function Home() {
   return (
     <>
-      <div class="flex flex-col justify-around min-h-screen">
+      <div class="flex flex-col min-h-screen">
         <div class="flex flex-col flex-grow justify-around">
           <div>
-          <img class="my-5" src="./src/assets/logo.svg" alt="Temnițe și Dragoni" />
-          <p class="text-2xl text-center tracking-wide text-gray-500 font-normal">Trăim mii de aventuri împreună!</p>
+            <img class="my-5" src="./src/assets/logo.svg" alt="Temnițe și Dragoni" />
+            <p class="text-2xl text-center tracking-wide text-gray-500 font-normal">Trăim mii de aventuri împreună!</p>
           </div>
         </div>
-        <div class="mb-8 mx-auto justify-center">
+        <div class="mb-8">
           <img src="./src/assets/iframe_example.png" alt="ig" />
         </div>
       </div>

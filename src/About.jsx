3626718ff703382757fa51d31181d2c05eb9e3b4
Renat Babacaev <renat.babacaev@gmail.com>
2024-12-13 12:42:24 +0200

    Remove uneccesary class

--- src/About.jsx
+++ src/About.jsx
@@ -2,12 +2,11 @@ function About() {
   return (
     <>
       <div class="flex flex-col justify-evenly min-h-screen">
-        <div class="mx-auto">
           <p class="text-xl text-justify">
             Temnițe și Dragoni MD este o grupă colectivă de jucători 
             formată din voluntari și jucători care au ca scop promovarea 
-            jocului de masă Dungeons and Dragons.</p>
-        </div>
+            jocului de masă Dungeons and Dragons.
+          </p>
       </div>
     </>
   );

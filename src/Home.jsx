225688c75dc4dba03e90c52c535b5d9b1d133d37
Renat Babacaev <renat.babacaev@gmail.com>
2024-12-13 12:41:28 +0200

    Drop instagram feed implementation

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,15 +1,14 @@
 function Home() {
+  var obj = {"opener": {
+    "value": "<iframe type=\"text/html\" width='1px' height='1px' src='https://www.embedista.com/ok/instagramfeed1707' frameborder='0' allowFullScreen></iframe>"
+  }}
+
   return (
     <>
-      <div class="flex flex-col min-h-screen">
-        <div class="flex flex-col flex-grow justify-around">
-          <div>
-            <img class="my-5" src="./src/assets/logo.svg" alt="Temnițe și Dragoni" />
-            <p class="text-2xl text-center tracking-wide text-gray-500 font-normal">Trăim mii de aventuri împreună!</p>
-          </div>
-        </div>
-        <div class="mb-8">
-          <img src="./src/assets/iframe_example.png" alt="ig" />
+      <div class="flex flex-col flex-grow min-h-screen justify-around">
+        <div>
+          <img class="my-5" src="./src/assets/logo.svg" alt="Temnițe și Dragoni" />
+          <p class="text-2xl text-center tracking-wide text-gray-500 font-normal">Trăim mii de aventuri împreună!</p>
         </div>
       </div>
     </>
